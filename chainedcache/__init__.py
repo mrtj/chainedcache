4709a5da696ea494be0f6d9cb6ce3399fe7243b0
Janos Tolgyesi <janos.tolgyesi@gmail.com>
2021-02-11 15:39:38 +0100

    Add docstring to module

--- chainedcache/__init__.py
+++ chainedcache/__init__.py
@@ -1,1 +1,26 @@
+'''
+Simple python cache system.
+
+Example usage:
+
+    json2bytes = lambda d: json.dumps(d).encode('UTF-8')
+    bytes2json = lambda d: json.loads(d.decode('UTF-8'))
+    stream2json = lambda d: json.load(d)
+
+    dict_cache = DictCache()
+
+    file_cache = FileCache('./cache', mode='bytes', 
+                           put_transformer=json2bytes, get_transformer=bytes2json)
+
+    s3_cache = S3Cache('my_s3_bucket', 'cache', region='us-east-1', 
+                       put_transformer=json2bytes, get_transformer=stream2json)
+
+    cache = ChainedCache([dict_cache, file_cache, s3_cache])
+
+    def data_generator(key):
+        return { "the_key_is": key }
+
+    json_data = cache.get_put("hello", data_generator)
+'''
+
 from .chainedcache import DictCache, FileCache, S3Cache, ChainedCache
